65c02f20c8372827542d2e5cc4b47b13b099951b
Usama Mansoor <usamamansoor026@gmail.com>
2024-06-30 12:19:01 +0500

    Navbar added complete responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const Navbar = () => {
-  return (
-    <header className="px-7 py-7">
-      <div className="flex items-center justify-between">
-        {/* Social Links */}
-        <ul className="flex items-center gap-5">
-          <li className="icon text-xl duration-300 hover:text-primary-color">
-            <a href="/">
-              <ion-icon name="logo-facebook"></ion-icon>
-            </a>
-          </li>
-          <li className="icon text-xl duration-300 hover:text-primary-color">
-            <a href="/">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-          </li>
-          <li className="icon text-xl duration-300 hover:text-primary-color">
-            <a href="/">
-              <ion-icon name="logo-twitter"></ion-icon>
-            </a>
-          </li>
-        </ul>
-        <h1 className="shopName text-6xl italic text-primary-color cursor-pointer">
-          Maan Cakes
-        </h1>
-        <Button
-          text="Call Us: +92 334 5137178"
-          bgColor="transparent"
-          border="true"
-        />
-      </div>
+  const [mobileNav, setMobileNav] = useState(false);
 
-      {/* Links */}
-      <ul>
-        <li>Home</li>
-        <li>cakes</li>
-        <li>about</li>
-        <li>blog</li>
-        <li>contact</li>
-      </ul>
-    </header>
+  const handleNav = () => {
+    setMobileNav(!mobileNav);
+  };
+
+  return (
+    <>
+      <header className="px-16 lg:px-7 py-7 relative">
+        <div className="flex items-center justify-between">
+          {/* Social Links */}
+          <ul className="flex items-center gap-5">
+            <li className="icon text-xl duration-300 hover:text-primary-color">
+              <a href="/">
+                <ion-icon name="logo-facebook"></ion-icon>
+              </a>
+            </li>
+            <li className="icon text-xl duration-300 hover:text-primary-color">
+              <a href="/">
+                <ion-icon name="logo-instagram"></ion-icon>
+              </a>
+            </li>
+            <li className="icon text-xl duration-300 hover:text-primary-color">
+              <a href="/">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </a>
+            </li>
+          </ul>
+          <h1 className="shopName text-6xl italic text-primary-color cursor-pointer">
+            Maan Cakes
+          </h1>
+          <div className="hidden lg:block">
+            <Button
+              text="Call Us: +92 334 5137178"
+              bgColor="transparent"
+              border="true"
+            />
+          </div>
+        </div>
+
+        {/* Links */}
+        <ul
+          className={`${
+            mobileNav ? "active" : "inactive"
+          } navList hidden lg:flex items-center justify-center gap-6 mt-10 mb-5`}
+        >
+          <li className="link afterLine">Home</li>
+          <li className="link afterLine">cakes</li>
+          <li className="link afterLine">about</li>
+          <li className="link afterLine">blog</li>
+          <li className="link afterLine">contact</li>
+        </ul>
+      </header>
+
+      {/* Hamburger Menu */}
+      <div className="flex justify-end lg:hidden bg-nav-link-bg py-3 ">
+        <div className="flex items-center gap-1 mr-3 bg-light-black px-2 py-0.5 rounded-[2px]">
+          <strong className="uppercase">Menu</strong>
+          <span
+            onClick={handleNav}
+            className="flex justify-center items-center text-xl"
+          >
+            <ion-icon name="menu"></ion-icon>
+          </span>
+        </div>
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,22 @@ export default {
       colors: {
         "primary-color": "#E5BF4A",
         "text-color": "#2a2a2a",
+        "light-black": "#222222",
         "sec-text": "#0a0a0a",
         "light-gray": "#dfdfdf",
+        "nav-link-bg": "#4c4c4c",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // ...
+    function ({ addUtilities }) {
+      addUtilities({
+        ".link": {
+          "@apply px-2 text-white relative font-semibold uppercase cursor-pointer hover:text-primary-color":
+            "",
+        },
+      });
+    },
+  ],
 };
